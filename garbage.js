86cde1241a740fd83aa921f39c585d9b0ec60267
Rishabh Raj <152422614+Rishabh-J08@users.noreply.github.com>
2024-11-17 12:15:26 +0530

    Delete garbage.js

--- garbage.js
+++
@@ -1,4 +0,0 @@
-const otpGenerator = require('otp-generator')
-
-const otp = otpGenerator.generate(4,{upperCaseAlphabets: false, specialChars:false ,lowerCaseAlphabets: false, digits:true});
-console.log(otp);
